d9054c3658b17ed63aff3007221f0e9b99ead9cd
fadhilagusta <32612244+fadhilagusta@users.noreply.github.com>
2017-10-13 15:49:11 +0400

    Update Get_visual.py

--- Get_visual.py
+++ Get_visual.py
@@ -5,27 +5,25 @@ import seaborn as sns
 
 dir = "input"
 
-#from subprocess import check_output
-#print(check_output(["cd", dir]).decode("utf8"))
 
 import glob
 
 
 data = pd.DataFrame()
 
-for f in glob.glob(dir+'\Indiegogo_2016-05-14.csv'):
+for f in glob.glob(dir+'\Indiegogo_2016-06-14.csv'):
 
-    #print (f)
-
+   
     data = pd.concat([data,pd.read_csv(f,low_memory=False)])
 
-# all files in the directory that matchs the criteria.
+
 
 # DATA CLEANING
 
 useless_columns = ["id","url","category_url","igg_image_url","compressed_image_url","card_type",
                    "category_slug","partner_name","source_url","friend_team_members","friend_contributors"]
 data = data.drop(useless_columns, axis = 1)
+
 
 import re
 def Remove_Non_Numeric (column) :
@@ -66,7 +64,7 @@ data.in_forever_funding.unique()
 
 
 
-# \\\\\ DATA VISUALIZATION  ///// NOT SOLVED
+# DATA VISUALIZATION
 
 data.balance = data.balance.apply(lambda row: float(row))
 
@@ -74,9 +72,11 @@ data.balance = data.balance.apply(lambda row: float(row))
 def sb_BarPlot(data,label,measure):
     a4_dims = (11.0, 8.27)
     fig, ax = plt.subplots(figsize=a4_dims)
-    plot = sns.barplot(y=label, x=measure,ax=ax, data=data,orient="vertical")
+    plot = sns.barplot(y=label, x=measure,ax=ax, data=data,orient="horizontal")
 
- sb_BarPlot(data,"category_name","balance")
+sb_BarPlot(data,"category_name","balance")
+
+plt._show()
 
 
-print(data.head())
+
